533d114179884282471a58ed475be8c81b7d98f0
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2021-10-19 23:40:57 +0700

    Finish the exercise one until ten from book Learn python 3 the hard way by Zed A Saw

---
+++ HARD WAY 1/exercise2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read tour program letter
+# Anyhting after the # is ignored by pyhton
+
+print("I could have code like this.") # and the comment after is ignored
+
+#you can also use a comment to "disabele" or comment out code.
+
+print("This will run.")
---
+++ HARD WAY 1/exercise5.py
@@ -0,0 +1,20 @@
+#MORE VARIABLES AND PRINTING
+
+my_name = 'Felicio N V B FERNANDES'
+my_age = 27 # it's a lie
+my_height = 170 # cm
+my_weight = 85 # cm
+my_eyes = 'Black'
+my_teeth = 'white'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy")
+print(f"Actually that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
