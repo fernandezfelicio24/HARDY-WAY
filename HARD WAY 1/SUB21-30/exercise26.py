5342def0e59b6529de98fd3645fbf30d2de7441b
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-06-27 12:36:39 +0700

    Test

--- HARD WAY 1/SUB21-30/exercise26.py
+++ HARD WAY 1/SUB21-30/exercise26.py
@@ -1,6 +1,12 @@
 import exercise25
 
 sentence = "All good things come to those who wait"
-
+"""
 words = exercise25.break_words(sentence)
+print(words)
+
+words = exercise25.sort_words(sentence)
+print(words)
+"""
+words = exercise25.print_first_word(sentence)
 print(words)
