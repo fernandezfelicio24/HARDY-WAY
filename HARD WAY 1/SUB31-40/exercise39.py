40a58700869e2ceb5c92033de43cab5042593449
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-07-01 08:37:24 +0700

    Continue Exercise 36 and 39

--- HARD WAY 1/SUB31-40/exercise39.py
+++ HARD WAY 1/SUB31-40/exercise39.py
@@ -40,3 +40,24 @@ print("Florida has:", cities[states['Florida']])
 
 #print every state abbreviation
 print('-' * 40)
+for state, abbrev in list(states.items()):
+    print(f"{state} has the city {abbrev}")
+    
+#print evry city in state
+print('-' * 40)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the {city}")
+    
+#now do both at the same time
+print('-' * 40)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+    
+print('-' * 40)
+#safely get a abbreviation by state that might not be there
+state = states.get('Texas.')
+
+#get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for the state 'TX' is: {city}")
