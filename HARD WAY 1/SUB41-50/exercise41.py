988598d8fd3e0bd9b2c10029b34cc8031c337e7b
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-07-05 12:09:30 +0700

    Exercise41

---
+++ HARD WAY 1/SUB41-50/exercise41.py
@@ -0,0 +1,12 @@
+# LEARNING TO SPEAK OBJECT-ORIENTED
+
+import random
+from urllib.request import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+    
+}
